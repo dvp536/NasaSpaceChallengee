a02cfae5dc6a90f67455c904f68927b957a1fdd7
dvp536 <missdegat@hotmail.com>
2024-10-06 16:20:47 -0400

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
  
     return (
         <div>
-            <Router basename="/https://dvp536.github.io/NasaSpaceChallengee/">
+            <Router basename="/NasaSpaceChallengee">
                 <Routes>
                     <Route path="./" element={<Home />} />
                     <Route path="./mars" element={<MarsPage />} />
